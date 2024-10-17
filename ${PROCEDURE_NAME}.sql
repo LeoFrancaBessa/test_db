662067968937b13f5bd1e817eae54369676753f3
teste <teste@teste.com>
2024-10-17 08:20:05 -0300

    Adicionado arquivo da procedure SQL

---
+++ ${PROCEDURE_NAME}.sql
@@ -0,0 +1,9 @@
+
+        DELIMITER //
+        CREATE PROCEDURE ${PROCEDURE_NAME}()
+        BEGIN
+            -- Defina sua lógica aqui
+            SELECT 'Hello, world!' AS message;
+        END //
+        DELIMITER ;
+        
