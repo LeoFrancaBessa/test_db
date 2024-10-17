0ec701196213f6eac290a1921a84ea3eb8b0b2d5
teste <teste@teste.com>
2024-10-17 10:57:46 -0300

    Adicionado arquivo da procedure SQL

---
+++ minha_procedure.sql
@@ -0,0 +1,9 @@
+
+        DELIMITER //
+        CREATE PROCEDURE minha_procedure()
+        BEGIN
+            -- Defina sua lógica aqui
+            SELECT 'Hello, world!!' AS message;
+        END //
+        DELIMITER ;
+        
